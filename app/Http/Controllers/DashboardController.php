01c4735f6e4d30ce9a0f21738da4eadb510dd39d
Hadley <hadley.markoski@gmail.com>
2019-02-28 19:21:17 +0000

    Created a plans database and model. Linked it to user

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+use App\Plan;
+use App\User;
 
 class DashboardController extends Controller
 {
 
 	public function index()
 	{
-		return view('dashboard');
+		Log::debug('User id is '.Auth::id());
+		$plans = User::find(Auth::id())->plans;
+		$viewData['plans'] = $plans;
+		return view('dashboard', $viewData);
 	}
 
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,5 +3,10 @@
 @section('content')
 <div class="container">
 	<p>Yo dawg I heard you like dashboards</p>
+	@if ($plans->count() === 0)
+		<p>:( You don't have any plans set up</p>
+	@else
+		<p>Here's a list of your plans</p>
+	@endif
 </div>	
 @endsection
