244b5a41e65a2e17b0e808bdcdd1c25ee497840d
Hadley <hadley.markoski@gmail.com>
2019-03-01 16:34:52 +0000

    Another test commit

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -va ./ ubuntu@fitness.silentrunning.info:/var/www/html/fitness-dev/ --exclude '.git*' --exclude 'bootstrap*' --exclude 'storage*'
+rsync -va ./ ubuntu@fitness.silentrunning.info:/var/www/html/fitness-dev/ --exclude '.git*'
